14d9616ce1f965dd5efc9d84ba8f352c1079df45
Brian Topping <topping@google.com>
2020-08-28 12:54:41 -0600

    Initial import

---
+++ src/test/java/com/example/jx3projecttest/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jx3projecttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
